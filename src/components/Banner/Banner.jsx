4f76ce9648e59dcae07499ae4af45b7956485431
Showvike Mondal Ovi <showvike@gmail.com>
2023-09-06 19:59:40 +0600

    banner done

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import banner from "../../assets/banner.svg";
+
+const Banner = () => {
+  return (
+    <div className="grid grid-cols-2 justify-between items-center pt-24 px-28 bg-[#f5f5f5]">
+      <div>
+        <img src={banner} alt="banner" />
+      </div>
+      <div>
+        <h1 className="text-4xl font-black text-[#444]">
+          Stay Safe, Stay Healthy
+        </h1>
+        <p className="text-lg mt-2 pt-2 text-[#777]">
+          Stay healthy and stay safe - two vital mantras for a secure, vibrant
+          life in a world filled with uncertainties.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/logo/logo.png";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between items-center h-24 shadow-md px-28 fixed w-full">
+    <nav className="flex justify-between items-center h-24 shadow-md px-28 fixed w-full bg-white">
       <div>
         <Link to="#">
           <img className="w-24 h-24" src={logo} alt="logo.png" />
