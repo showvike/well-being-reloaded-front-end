160ec94db8a7891f51828036ce5f2743414b62ea
Showvike Mondal Ovi <showvike@gmail.com>
2023-09-06 23:10:08 +0600

    footer done

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import About from "../../components/About/About";
 import Banner from "../../components/Banner/Banner";
 import Doctors from "../../components/Doctors/Doctors";
+import Footer from "../../components/Footer/Footer";
 import Nav from "../../components/Nav/Nav";
 import Services from "../../components/Services/Services";
 import Statistics from "../../components/Statistics/Statistics";
@@ -14,6 +15,7 @@ const Landing = () => {
       <Services />
       <About />
       <Doctors />
+      <Footer />
     </div>
   );
 };
